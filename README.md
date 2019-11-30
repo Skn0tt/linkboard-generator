3ffaaae2da18a27bce9c60337c3521bb8f58abe4
Simon Knott <simoknott@gmail.com>
2019-11-30 11:23:46 +0100

    Create cli interface

---
+++ README.md
@@ -0,0 +1,7 @@
+# Usage
+
+```
+echo '[{ ... your servers here  ... }]' | docker run -i -v ./outfolder:/dist server-selector-generator
+```
+
+find your files in `outfolder`
---
+++ index.js
@@ -0,0 +1,48 @@
+const Bundler = require("parcel-bundler");
+const path = require("path");
+const fs = require("fs");
+const assert = require("assert");
+
+function validateStdin(stdin) {
+  const arr = JSON.parse(stdin);
+  assert(Array.isArray(arr), "stdin must be array");
+  arr.forEach(v => {
+    const { acronym, name, logo, href, color } = v;
+    assert(typeof acronym === "string");
+    assert(typeof name === "string");
+    assert(typeof logo === "string");
+    assert(typeof href === "string");
+    assert(typeof color === "string");
+  });
+}
+
+function writeInputJson() {
+  const stdin = fs.readFileSync(0, "utf-8");
+  validateStdin(stdin);
+  const inputJson = path.join(__dirname, "./src/input.json");
+  fs.writeFileSync(inputJson, stdin);
+}
+
+const entryFiles = path.join(__dirname, "./src/index.html");
+
+const withProductionEnv = task => async () => {
+  const oldEnv = process.env.NODE_ENV;
+  process.env.NODE_ENV = "production";
+  await task();
+  process.env.NODE_ENV = oldEnv;
+}
+
+const bundle = withProductionEnv(async () => {
+  const bundler = new Bundler(entryFiles);
+  await bundler.bundle();
+})
+
+async function main() {
+  writeInputJson();
+  await bundle();
+}
+
+main();
+
+
+
